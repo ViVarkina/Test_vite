6cb511c18ecc86acc8c236973adc85be952cf83e
ViVarkina <vikavarkina@gmail.com>
2024-06-16 14:00:02 +0300

    16.06 изменение названия тодолиста

--- src/body/components/todoLists/components/todoList/TodoList.tsx
+++ src/body/components/todoLists/components/todoList/TodoList.tsx
@@ -2,13 +2,15 @@ import {FilterBlock} from "../fiterBlock/FilterBlock.tsx";
 import {TasksList} from "../tasksList/TasksList.tsx";
 import {AddTask} from "../addTask/AddTask.tsx";
 import {Dispatch, SetStateAction, useState} from "react";
-import {TaskType} from "../../TodoLists.tsx";
+import {TaskType, TodoListsType} from "../../TodoLists.tsx";
+import {ChangeTitle} from "../changeTitile/ChangeTitle.tsx";
 
 interface PropsType {
     title: string
     tasks: Task[]
     setTasks: Dispatch<SetStateAction<TaskType>>
     todolistId: string
+    setTodolists: Dispatch<SetStateAction<TodoListsType[]>>
 }
 
 export interface Task {
@@ -16,10 +18,11 @@ export interface Task {
     task: string
     isDone: boolean
     todolistId: string
+
 }
 
 export type FilterStateType = "All" | "Active" | "Closed"
-export const TodoList = ({title, tasks, setTasks, todolistId}: PropsType) => {
+export const TodoList = ({title, tasks, setTasks, todolistId, setTodolists}: PropsType) => {
 
 
     const [filterState, setFilterState] = useState<FilterStateType>("All")
@@ -35,7 +38,7 @@ export const TodoList = ({title, tasks, setTasks, todolistId}: PropsType) => {
 
 
     return <>
-        <div>{title}</div>
+        <ChangeTitle title={title} todolistId={todolistId} setTodolists={setTodolists}/>
         <AddTask setTasks={setTasks} todolistId={todolistId}/>
         <TasksList setTasks={setTasks} filterTask={filterTask} todolistId={todolistId}/>
         <FilterBlock setFilterState={setFilterState} filterState={filterState}/>
