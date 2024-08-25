98c5d4ef018868a0c6a01dbb548c832984359984
ViVarkina <vikavarkina@gmail.com>
2024-08-25 12:49:35 +0300

    migrate to context 2

--- src/body/components/todoLists/components/addTodolist/AddTodoList.tsx
+++ src/body/components/todoLists/components/addTodolist/AddTodoList.tsx
@@ -1,28 +1,17 @@
 import { useContext, useState } from 'react';
-import { v4 as uuisv4 } from 'uuid';
 
 import { BaseButton, BaseInput } from '@/shared';
-import { TodoListsType } from '@/type';
 import { TodolistContext } from '@/App/provioder';
 
 export const AddTodoList = () => {
   const [value, setValue] = useState<string>('');
-  const { setTodolists, setTasksObj: setTasks } = useContext(TodolistContext);
+  const { addTodolist } = useContext(TodolistContext);
+  const onClear = () => {
+    setValue(' ');
+  };
 
-  const onClickAddTodolist = () => {
-    const todoListId = uuisv4();
-    const newTodoList: TodoListsType = {
-      id: todoListId,
-      title: value,
-    };
-    const newTask = {
-      [todoListId]: [],
-    };
-    setTodolists((prevState) => [newTodoList, ...prevState]);
-    setTasks((prevState) => {
-      return { ...prevState, ...newTask };
-    });
-    setValue('');
+  const onClick = () => {
+    addTodolist(value, onClear);
   };
 
   return (
@@ -35,7 +24,7 @@ export const AddTodoList = () => {
           }}
           value={value}
         />
-        <BaseButton onClick={onClickAddTodolist}>+</BaseButton>
+        <BaseButton onClick={onClick}>+</BaseButton>
       </div>
     </>
   );
